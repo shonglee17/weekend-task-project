022581754c1c27e78326d6068d09326e6b814047
Shong Lee <Shonglee17@gmail.com>
2022-12-21 21:48:44 -0600

    added swak to delete/remove buttons

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,92 +1,147 @@
-$(document).ready(onReady)
+$(document).ready(onReady);
 
-function onReady(){
-    renderTask()
-    $('#enterButton').on('click', addTask)
-    $('body').on('click', '.deleteButton',  deleteTask)
-    $('body').on('click', '.completeButton',  completeTask)
-
+function onReady() {
+  renderTask();
+  $('#enterButton').on('click', addTask);
+  $('body').on('click', '.deleteButton', deleteTask);
+  $('body').on('click', '.completeButton', completeTask);
 }
-function renderTask(){
-    $.ajax({
-        type: 'GET',
-        url: '/weekend-to-do-app'
-    }).then((res)=>{
-        $('#renderInfo').empty()
-        for (let jobs of res){
-            $('#renderInfo').append(`
+function renderTask() {
+  $.ajax({
+    type: 'GET',
+    url: '/weekend-to-do-app',
+  })
+    .then((res) => {
+      $('#renderInfo').empty();
+      for (let jobs of res) {
+        $('#renderInfo').append(`
             <tr ${conditionalBackgroundColor(jobs)}>
                 <td>${jobs.job}</td>
                 <td>${jobs.description}</td>
                 <td data-status=${jobs.status}>${jobs.status}</td>
+                <td>conditional calculation here</td>
                 <td><button class="completeButton" data-id=${jobs.id}>DONE</button></td>
                 <td><button class="deleteButton" data-id=${jobs.id}>DELETE</button></td>
             </tr>
-            `)
-        }
-    }).catch((error)=>{
-        console.log('GET route broke: client side', error);
+            `);
+      }
+      $('#jobInput').val('');
+      $('#descriptionInput').val('');
     })
-
-    $('#jobInput').val('')
-    $('#descriptionInput').val('')
+    .catch((error) => {
+      console.log('GET route broke: client side', error);
+    });
 }
 
-function addTask(){
+function addTask() {
+  let jobInput = $('#jobInput').val();
+  let descriptionInput = $('#descriptionInput').val();
 
-    let jobInput = $('#jobInput').val()
-    let descriptionInput = $('#descriptionInput').val()
-
-    let postNewJob = {
-        job: jobInput,
-        description: descriptionInput,
-        status: 'incomplete'
-    }
-    $.ajax({
-        method: 'POST',
-        url: '/weekend-to-do-app',
-        data: postNewJob
-      }).then ((response) => {
-        renderTask();
-      }).catch((error) => {
-        console.log('something broke in POST: clientside', error);
-      })
-
+  let postNewJob = {
+    job: jobInput,
+    description: descriptionInput,
+    status: 'INCOMPLETE',
+  };
+  $.ajax({
+    method: 'POST',
+    url: '/weekend-to-do-app',
+    data: postNewJob,
+  })
+    .then((response) => {
+      renderTask();
+    })
+    .catch((error) => {
+      console.log('something broke in POST: clientside', error);
+    });
 }
 
-function deleteTask(){
-    let deleteThis = $(this).data().id
-    $.ajax({
-      method: 'DELETE',
-      url: `/weekend-to-do-app/${deleteThis}`,
-    }).then((response)=>{
-      renderTask()
-    }).catch((error) =>{
+function deleteTask() {
+  Swal.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete it!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'Deleted!',
+        'This job has been deleted.',
+        'success'
+      )
+      let deleteThis = $(this).data().id;
+  $.ajax({
+    method: 'DELETE',
+    url: `/weekend-to-do-app/${deleteThis}`,
+  })
+    .then((response) => {
+      renderTask();
+    })
+    .catch((error) => {
       console.log('something broke in DELETE: client side', error);
-    })
+    });
+    }
+  })
 }
 
-function completeTask(){
-    let updateStatus = $(this).data().id
-    $.ajax({
-      method: 'PUT',
-      url: `/weekend-to-do-app/${updateStatus}`,
-      data: {
-        status: 'complete'
-        }
-    }).then((response)=>{
-      renderTask()
-    }).catch((error) =>{
-      console.log('something broke in PUT: client side', error);
-    })
+function completeTask() {
+  Swal.fire({
+    title: 'Submit your first name',
+    input: 'text',
+    inputAttributes: {
+      autocapitalize: 'off'
+    },
+    showCancelButton: true,
+    confirmButtonText: 'Enter',
+    showLoaderOnConfirm: true,
+    preConfirm: (login) => {
+      return fetch(`//api.github.com/users/${login}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(response.statusText)
+          
+          }
+          return response.json()
+        })
+        .catch(error => {
+          Swal.showValidationMessage(
+            `Request failed: ${error}`
+          )
+        })
+    },
+    allowOutsideClick: () => !Swal.isLoading()
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: `${result.value.login}'s avatar`,
+        imageUrl: result.value.avatar_url
+      })
+      let updateStatus = $(this).data().id;
+      $.ajax({
+        method: 'PUT',
+        url: `/weekend-to-do-app/${updateStatus}`,
+        data: {
+          status: 'COMPLETE',
+        },
+      })
+        .then((response) => {
+          
+          renderTask();
+        })
+        .catch((error) => {
+          console.log('something broke in PUT: client side', error);
+        });
+    }
+  })
+  
 }
 
 function conditionalBackgroundColor(jobs) {
-    if (jobs.status === 'complete') {
-      return 'class="background"'
-    }
-    else {
-      return 'class="default"'
-    }
+  if (jobs.status === 'COMPLETE') {
+    return 'class="background"';
+  } else {
+    return 'class="default"';
+  }
 }
-
--- server/server.js
+++ server/server.js
@@ -1,20 +1,15 @@
-
-
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
-const task = require('./routes/task.router.js')
-const pool = require('./modules/pool.js')
+const task = require('./routes/task.router.js');
 
-
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static('server/public'));
 
 // ROUTES
-app.use('/weekend-to-do-app', task)
+app.use('/weekend-to-do-app', task);
 
 // Start listening for requests on a specific port
 app.listen(PORT, () => {
